fad6ecc799c1cce73bd32058988801e36b225435
Balakrishna Ragala <baluragala@gmail.com>
2018-02-12 07:45:04 +0530

    iterator function override

--- 7.1-iterable and Iterator.js
+++ 7.1-iterable and Iterator.js
@@ -45,3 +45,24 @@ myIterable[Symbol.iterator] = function* () {
     yield 3;
 };
 [...myIterable]; // [1, 2, 3]
+
+// override iterator
+var numbers = [1, 2, 3, 4, 5];
+
+numbers[Symbol.iterator] = function() {
+  console.log(arguments);
+  var index = 0;
+  return {
+    next() {
+      if (index < numbers.length) {
+        return { value: numbers[index++], done: false };
+      } else {
+        return { value: undefined, done: true };
+      }
+    }
+  };
+};
+
+for (let n of numbers) {
+  console.log(n);
+}
