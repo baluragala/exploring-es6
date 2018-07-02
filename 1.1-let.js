3646e0b6775ec8e1a4ef92b63f399702519c69a1
Balakrishna Ragala <baluragala@gmail.com>
2018-07-02 22:33:56 +0530

    Update 1.1-let.js

--- 1.1-let.js
+++ 1.1-let.js
@@ -10,3 +10,10 @@ function func(randomize) {
     return x;
 }
 func(false); // 3
+
+for(let i=0;i<10;i++){
+  console.log(i); //i is visible thus is logged in the console as 0,1,2,....,9
+}
+console.log(i); //throws an error as "i is not defined" because i is not visible
+
+//re-declaration not allowed
